25691c362077e77ecbb312b9736a060e3e593018
brajesh <brajesh.singhania143@gmail.com>
2024-05-23 13:49:02 +0530

    add axios

--- src/Components/HomeGuest.js
+++ src/Components/HomeGuest.js
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import Page from "./Page"
-
+import axios from "axios"
 
 function HomeGuest() {
+  const [ userInput, setUser ] = useState({
+    fname: "",
+    lname: "",
+    username: "",
+    password: ""
+  })
+
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    setUser({...userInput, [name]: value })
+  }
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    
+    const userData = await axios.post('http://localhost:5000/api/user/signup', userInput)
+    console.log(userData) 
+  }
+ 
   return (
     <Page title="Welcome!" wide={true}>
       <div className="row align-items-center">
@@ -11,33 +30,53 @@ function HomeGuest() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSignup}>
             <div className="form-group">
               <label htmlFor="fname-register" className="text-muted mb-1">
                 <small>FirstName</small>
               </label>
-              <input id="fname-register" name="fname" className="form-control" type="text" 
-              autoComplete="off" />
+              <input id="fname-register" 
+              name="fname" 
+              className="form-control" 
+              type="text" 
+              autoComplete="off"
+              value={userInput.fname}
+              onChange={handleInput} />
             </div>
             <div className="form-group">
               <label htmlFor="lname-register" className="text-muted mb-1">
                 <small>LastName</small>
               </label>
-              <input id="lname-register" name="lanme" className="form-control" type="text"            
-              autoComplete="off" />
+              <input id="lname-register" 
+              name="lname" 
+              className="form-control" 
+              type="text"            
+              autoComplete="off"
+              value={userInput.lname} 
+              onChange={handleInput} />
             </div>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>UserName</small>
               </label>
-              <input id="username-register" name="username" className="form-control" type="text" />
+              <input id="username-register" 
+              name="username" 
+              className="form-control" 
+              type="text"
+              value={userInput.username}
+              onChange={handleInput} />
             </div>
 
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
-              <input id="password-register" name="password" className="form-control" type="password" />
+              <input id="password-register" 
+              name="password" 
+              className="form-control" 
+              type="password"
+              value={userInput.password}
+              onChange={handleInput} />
             </div>
 
             <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block" 
