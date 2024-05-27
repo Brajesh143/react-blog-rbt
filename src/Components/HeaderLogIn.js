99afeec6946f85fe0f6788758fabd3266783aedc
Brajesh Singh <brajesh_singh@persistent.com>
2024-05-27 17:17:21 +0530

    login and logout functionality

--- src/Components/HeaderLogIn.js
+++ src/Components/HeaderLogIn.js
@@ -1,17 +1,84 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
+import swal from "sweetalert"
 
 function HeaderLogIn(props) {
+  const [userInput, setUser] = useState({
+    username: "",
+    password: ""
+  })
+
+  const [errors, setErrors] = useState({})
+
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    setUser({...userInput, [name]: value })
+  }
+
+  const validation = () => {
+    let newErrors = {}
+    if (!userInput.username.trim()) newErrors.username = 'username is required'
+    if (!userInput.password.trim()) {
+      newErrors.password = 'password is required'
+    } else if (userInput.password.length < 8) {
+      newErrors.password = 'password must be at least 8 characters'
+    }
+
+    setErrors(newErrors)
+    return Object.keys(newErrors).length === 0;
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    if (validation()) {
+      const userData = await axios.post('http://localhost:5000/api/user/login', userInput)
+      if (userData.status === 200) {
+        props.setisUserLoggedIn(true)
+        const resData = userData.data 
+        const user_data_to_save = {
+          fname: resData.data.fname,
+          lname: resData.data.lname,
+          username: resData.data.username
+        }
+
+        const token = resData.token
+        localStorage.setItem('user', JSON.stringify(user_data_to_save))
+        localStorage.setItem('token', token)
+        
+        swal("Success!", resData.message, "success");
+
+        setUser({
+          username: '',
+          password: ''
+        })
+      }
+    }
+  }
+
   return (
-    <form className="mb-0 pt-2 pt-md-0">
+    <form className="mb-0 pt-2 pt-md-0" onSubmit={handleLogin}>
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+          <input name="username" 
+          className="form-control form-control-sm input-dark" 
+          type="text" 
+          placeholder="Username" 
+          autoComplete="off"
+          value={userInput.username}
+          onChange={handleInput} />
+          {errors.username && <span>{errors.username}</span>}
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input  name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+          <input  name="password" 
+          className="form-control form-control-sm input-dark" 
+          type="password" 
+          placeholder="Password"
+          value={userInput.password} 
+          onChange={handleInput} />
+          {errors.password && <span>{errors.password}</span>}
         </div>
         <div className="col-md-auto">
-          <button className="btn btn-success btn-sm">Sign In</button>
+          <button type="submit" className="btn btn-success btn-sm">Sign In</button>
         </div>
       </div>
     </form>
--- src/Components/routesFile/PublicRoute.js
+++ src/Components/routesFile/PublicRoute.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import About from '../About'
 import Blog from '../Blog'
+import CreateBlog from '../CreateBlog'
 import HomeGuest from '../HomeGuest'
+import MyBlog from '../MyBlog'
+import MyProfile from '../MyProfile'
 import Terms from '../Terms'
 
 export default function PublicRoute() {
   return (
     <Routes>
-        <Route path='/'element={<HomeGuest />} />
-        <Route path='/about-us'element={<About />} />
-        <Route path='/terms'element={<Terms />} />
-        <Route path='/blog'element={<Blog />} />
+        <Route path='/' element={<HomeGuest />} />
+        <Route path='/about-us' element={<About />} />
+        <Route path='/terms' element={<Terms />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-blog' element={<MyBlog />} />
+        <Route path='/create-blog' element={<CreateBlog />} />
     </Routes>
   )
 }
