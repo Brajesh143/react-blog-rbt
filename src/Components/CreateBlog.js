5ab2ef3e73b249f03003024ac103fae780b4b742
Brajesh Singh <brajesh_singh@persistent.com>
2024-05-30 15:26:58 +0530

    create blog and my profile functionality

--- src/Components/CreateBlog.js
+++ src/Components/CreateBlog.js
@@ -1,12 +1,66 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import swal from "sweetalert";
 
 export default function CreateBlog() {
+    const [blogInput, setBlogData] = useState({
+        title: "",
+        category: "",
+        description: ""
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const handleInput = (e) => {
+        setBlogData({...blogInput, [e.target.name] : e.target.value})
+    }
+
+    const validation = () => {
+        let newError = {}
+        if (!blogInput.title.trim()) newError.title = "title is required"
+
+        if (!blogInput.category.trim()) newError.category = "category is required"
+
+        if (!blogInput.description) newError.description = "description is required"
+
+        setErrors(newError)
+        return Object.keys(newError).length === 0;
+    }
+
+    const handleCreateBlog = async (e) => {
+        e.preventDefault()
+
+        if (validation()) {
+            const token = localStorage.getItem('token')
+            let config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: 'http://localhost:5000/api/blog/create',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                data: blogInput
+            };
+            const createBlog = await axios.request(config)
+            const resData = createBlog.data
+            if (createBlog.status === 201) {
+                swal("Success!", resData.message, "success");
+
+                setBlogData({
+                    title: '',
+                    category: '',
+                    description: ''
+                })
+            }
+        }
+    }
+
     return (
         <>
             <div className="container">
                 <h3 className="text-center">Create Blog</h3>
                 <div className="col-lg-12">
-                    <form>
+                    <form onSubmit={handleCreateBlog}>
                         <div className="form-group">
                             <label htmlFor="title" className="text-muted mb-1">
                                 <small>Title</small>
@@ -16,7 +70,10 @@ export default function CreateBlog() {
                             className="form-control" 
                             type="text" 
                             autoComplete="off"
+                            value={blogInput.title}
+                            onChange={handleInput}
                             />
+                            {errors.title && <span>{errors.title}</span>}
                         </div>
                         <div className="form-group">
                             <label htmlFor="category" className="text-muted mb-1">
@@ -27,7 +84,10 @@ export default function CreateBlog() {
                             className="form-control" 
                             type="text"            
                             autoComplete="off"
+                            value={blogInput.category}
+                            onChange={handleInput}
                             />
+                            {errors.category && <span>{errors.category}</span>}
                         </div>
                         <div className="form-group">
                             <label htmlFor="description" className="text-muted mb-1">
@@ -40,7 +100,10 @@ export default function CreateBlog() {
                                 rows={4}
                                 type="text"            
                                 autoComplete="off"
+                                value={blogInput.description}
+                                onChange={handleInput}
                             />
+                            {errors.description && <span>{errors.description}</span>}
                         </div>
 
                         <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -1,10 +1,83 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function MyProfile() {
+    const [userData, setUserData] = useState({
+        fname: "",
+        lname: "",
+        username: ""
+    })
+
+    useEffect(() => {
+        getUserData()
+    }, []);
+
+    const getUserData = async () => {
+        const token = localStorage.getItem('token')
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:5000/api/user/user-profile',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        };
+        const userData = await axios.request(config)
+        if (userData.status === 200) {
+            const resData = userData.data.data;
+            setUserData(resData)
+        }
+    }
+
     return (
         <>
-            <div>
-                My Profile
+            <div className="container">
+                <h3 className="text-center">My Profile</h3>
+                <div className="col-lg-12">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="fname" className="text-muted mb-1">
+                                <small>First Name</small>
+                            </label>
+                            <input id="fname" 
+                            name="fname" 
+                            className="form-control" 
+                            type="text" 
+                            autoComplete="off"
+                            value={userData.fname}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="lname" className="text-muted mb-1">
+                                <small>Last Name</small>
+                            </label>
+                            <input id="lname" 
+                            name="lname" 
+                            className="form-control" 
+                            type="text"            
+                            autoComplete="off"
+                            value={userData.lname}
+                            />
+                        </div>
+                        
+                        <div className="form-group">
+                            <label htmlFor="username" className="text-muted mb-1">
+                                <small>Username</small>
+                            </label>
+                            <input id="username" 
+                            name="username" 
+                            className="form-control" 
+                            type="text"            
+                            autoComplete="off"
+                            value={userData.username}
+                            />
+                        </div>
+
+                        <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
+                        Update
+                        </button>
+                    </form>
+                </div>
             </div>
         </>
     )
