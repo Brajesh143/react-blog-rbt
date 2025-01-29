743261651e5d7de90ee672109156c37f47eb3db7
brajesh <brajesh.singhania143@gmail.com>
2025-01-29 11:01:20 +0530

    Call api in product component.

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,12 +1,30 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { Link } from 'react-router-dom'
 import PaginationComponent from './PaginationComponent';
+import axios from 'axios';
 
 export default function Product() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts()
+  }, []);
+
+  const getProducts = async() => {
+    const url = 'http://localhost:5000/api/product';
+    const productResponse = await axios.get(url);
+    if (productResponse.status === 200) {
+      console.log(productResponse.data.data)
+      setProducts(productResponse.data.data);
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className='container'>
         <div className='row'>
@@ -29,75 +47,26 @@ export default function Product() {
           </div>
         </div>
         <div className='row'>
-          <div className='col-md-4'>
-            <Card style={{ width: '24rem' }}>
-              <Card.Img variant="top" src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-
-          <div className='col-md-4'>
-            <Card style={{ width: '24rem' }}>
-              <Card.Img variant="top" src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-
-          <div className='col-md-4'>
-            <Card style={{ width: '24rem' }}>
-              <Card.Img variant="top" src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-
-          <div className='col-md-4'>
-            <Card style={{ width: '24rem' }}>
-              <Card.Img variant="top" src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-
-          <div className='col-md-4'>
-            <Card style={{ width: '24rem' }}>
-              <Card.Img variant="top" src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
+          { products.length > 0 && products.map(product => (
+            <div className='col-md-4'>
+              <Card style={{ width: '24rem' }}>
+                <Card.Img variant="top" src={`http://localhost:5000${product.product_image || '/public/uploads/products/dummy_product.jpg'}`} height={250} />
+                <Card.Body>
+                  <Card.Title>{product?.name}</Card.Title>
+                  <Card.Text style={{fontSize: "large", height: "70px"}}>
+                    {product?.description}
+                  </Card.Text>
+                  <Card.Text style={{fontSize: "x-large", height: "45px"}}>
+                    {`$ ${product?.price}`}
+                  </Card.Text>
+                  <div>
+                    <Button variant="primary" style={{ marginRight: "100px" }}>Add to Cart</Button>
+                    <Button>Add to Wishlist</Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
         </div>
 
         <PaginationComponent />
